5b0b03d80c5bf9ab1ad320a99f4ac4d5417ce8bb
shubhali11 <shubhali11@gmail.com>
2025-08-17 08:24:09 +0530

    Configure project for Render deployment

--- lms/settings.py
+++ lms/settings.py
@@ -21,12 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*pohz@)x(w@smnfyo(&4mqx)jk@i*rde%p&j*ylk!bzf94t4s&'
-
+#SECRET_KEY = 'django-insecure-*pohz@)x(w@smnfyo(&4mqx)jk@i*rde%p&j*ylk!bzf94t4s&'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-*pohz@)x(w@smnfyo(&4mqx)jk@i*rde%p&j*ylk!bzf94t4s&')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+import os
 
 ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
